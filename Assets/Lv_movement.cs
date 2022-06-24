4a7b22868c8dcf45ac1124f0e9260463e3d7bebe
trainman1974 <trainman1974@gmail.com>
2022-06-24 14:26:02 +0300

    firstcommit

---
+++ Assets/Lv_movement.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+
+public class Lv_movement : MonoBehaviour
+{
+    int Rnd;
+    int Derection = 0;
+    public GameObject Pre1;
+    public GameObject Pre2;
+    public GameObject Pre3;
+    public GameObject Background;
+    public Rigidbody Rb;
+    public AudioSource Cn;
+    public AudioSource Jp;
+    public Text Tx;
+    int Score;
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 0, 10) * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (Derection > -1)
+            {
+                Derection = Derection - 1;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (Derection < 1)
+            {
+                Derection = Derection + 1;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if ((transform.position.y<0.6)||(transform.position.y > 4.8)&&(transform.position.y < 4.9))
+            {
+                Jp.Play(0);
+                Rb.AddForce(0, 2000, 0);
+            }
+        }
+        transform.position = Vector3.Lerp(transform.position, new Vector3(Derection*3, transform.position.y, transform.position.z), Time.deltaTime*5);
+        Tx.text = Score.ToString();
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Coin")
+        {
+            Destroy(col.gameObject);
+            Cn.Play(0);
+            Score = Score + 1;
+        }
+        if (col.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("menu");
+        }
+        if (col.gameObject.tag == "Finish")
+        {
+            Rnd = Random.Range(1, 3);
+            switch (Rnd)
+            {
+                case 1:
+                    GameObject roadClone1 = Instantiate(Pre1, new Vector3(0, 0, transform.position.z + 175), transform.rotation);
+                    Destroy(roadClone1, 30f);
+                    break;
+                case 2:
+                    GameObject roadClone2 = Instantiate(Pre2, new Vector3(0, 0, transform.position.z + 175), transform.rotation);
+                    Destroy(roadClone2, 30f);
+                    break;
+                case 3:
+                    GameObject roadClone3 = Instantiate(Pre3, new Vector3(0, 0, transform.position.z + 175), transform.rotation);
+                    Destroy(roadClone3, 30f);
+                    break;
+            }
+            GameObject background = Instantiate(Background, new Vector3(0, 2, transform.position.z + 110), transform.rotation);
+            Destroy(background, 30f);
+        }
+    }
+}
---
+++ Assets/menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    public Button enterButton;
+    public Button exitButton;
+    void Start()
+    {
+        exitButton.onClick.AddListener(exit);
+        enterButton.onClick.AddListener(enter);
+    }
+    void exit()
+    {
+        Application.Quit();
+    }
+    void enter()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
